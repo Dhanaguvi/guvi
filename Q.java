99ae25794ebac4eeb034e7779a51b9658bcf9dff
Dhanaguvi <40167276+Dhanaguvi@users.noreply.github.com>
2018-06-27 09:45:06 +0530

    Create Q.java

---
+++ Q.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Q
+{
+public static void main(String[] str)
+    {
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    if(a>0)
+        {
+        System.out.print("positive");
+        }
+            if(a<0)
+            {
+                System.out.print("negative");
+            }
+        if(a==0)
+        {
+        System.out.print("zero");
+        }
+    }
+}
