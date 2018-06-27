91da95eb9bdc2b610d0ba5e112dd25cc2cf94195
Dhanaguvi <40167276+Dhanaguvi@users.noreply.github.com>
2018-06-27 10:46:11 +0530

    Update W.java

--- W.java
+++ W.java
@@ -1,20 +1,16 @@
 import java.util.*;
 class W
 {
-public static void main(String[] str)
-    {
+public static void main(String[] str){
     Scanner sc=new Scanner(System.in);
     int a=sc.nextInt();
-    if(a%2==0)
-        {
+    if(a%2==0){
         System.out.print("Even");
         }
-            if((a%2!=0)&&(a>0))
-            {
+            if((a%2!=0)&&(a>0)){
                 System.out.print("Odd");
             }
-        if(a<0)
-        {
+        if(a<0){
         System.out.print("invalid");
         }
     }
